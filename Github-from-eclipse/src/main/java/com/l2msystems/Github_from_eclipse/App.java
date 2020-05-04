cde80838c86e5caefdb85395c30100c020f60b0b
Fred Warsaw <fred.warsaw@yahoo.com>
2020-05-03 20:43:29 -0500

    First commit.

---
+++ Github-from-eclipse/src/main/java/com/l2msystems/Github_from_eclipse/App.java
@@ -0,0 +1,13 @@
+package com.l2msystems.Github_from_eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
